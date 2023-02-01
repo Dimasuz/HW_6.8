ae6b96bf21b7dc4c0db68a13778f571236fc05ca
Dimasuz <dimaskus@gmail.com>
2023-02-01 22:13:06 +0300

    HW_6.8.2

--- base_req.py
+++ base_req.py
@@ -1,8 +1,6 @@
-import atexit
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from models import People
-from pprint import pprint
 from config import PG_DSN
 
 engine = create_engine(PG_DSN)
@@ -10,14 +8,10 @@ engine = create_engine(PG_DSN)
 Session = sessionmaker(bind=engine)
 
 
-def get_people_list():
+def get_people_count():
     with Session() as session:
-        people_list = session.query(People.name).order_by(People.name).all()
-    return people_list
+        return session.query(People).count()
+
 
 if __name__ == '__main__':
-    people_list = get_people_list()
-    pprint(people_list)
-    print(len(people_list))
-
-    atexit.register(engine.dispose)
+    print(get_people_count())
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from more_itertools import chunked
 import requests
 from models import engine, Session, People, Base
 from config import CHUNK_SIZE, URL
+from base_req import get_people_count
 
 
 def max_people_count() -> int:
@@ -13,45 +14,35 @@ def max_people_count() -> int:
 
 async def get_item(i: int, client: ClientSession):
     k = 0
-    while i and k < 10:
+    item_json = None
+    while not item_json and k < 5:
         k += 1
-        # async with client.get(f'{URL}/{i}/') as item:
-        #     print(f'people_id {i}, status {item.status}, attempt {k} from 10')
-        #     if item.status == 200:
-        #         i = 0
-        #         item_json = await item.json()
-        item = await client.get(f'{URL}/{i}/')
-        print(f'people_id {i}, status {item.status}, attempt {k} from 10')
-        if item.status == 200:
-            i = 0
-            item_json = await item.json()
-            for key in ['created', 'edited', 'url']:
-                item_json.pop(key)
-            d = {'films': 'title', 'species': 'name', 'starships': 'name', 'vehicles': 'name'}
-            for k in d:
-                if item_json[k]:
-                    l = []
-                    for u in item_json[k]:
-                        res = await client.get(u)
-                        if res.status == 200:
-                            res_json = await res.json()
-                            l.append(res_json[d[k]])
-                    item_json[k] = ', '.join(l)
-                else:
-                    item_json[k] = ''
-        else:
-            item_json = None
+        async with client.get(f'{URL}/{i}/') as item:
+            print(f'people_id {i}, status {item.status}, attempt {k}')
+            if item.status == 200:
+                item_json = await item.json()
+                for key in ['created', 'edited', 'url']:
+                    item_json.pop(key)
+                d = {'films': 'title', 'species': 'name', 'starships': 'name', 'vehicles': 'name'}
+                for k in d:
+                    if item_json[k]:
+                        l = []
+                        for u in item_json[k]:
+                            res = await client.get(u)
+                            if res.status == 200:
+                                res_json = await res.json()
+                                l.append(res_json[d[k]])
+                        item_json[k] = ', '.join(l)
+                    else:
+                        item_json[k] = ''
+            else:
+                item_json = None
+
     return item_json
 
 
 async def add_to_db(items):
     async with Session() as session:
-        # list = [People(**item) for item in items if item]
-        # list = []
-        # for item in items:
-        #     if item:
-        #         list.append(People(**item))
-        # session.add_all(list)
         session.add_all([People(**item) for item in items if item])
         await session.commit()
 
@@ -62,14 +53,26 @@ async def main():
         await conn.run_sync(Base.metadata.create_all)
         await conn.commit()
     max_people = max_people_count()
-    async with ClientSession() as client:
-        for id_chunk in chunked(range(1, max_people + 2), CHUNK_SIZE):
-            items_caro = [get_item(i, client) for i in id_chunk]
-            items = await asyncio.gather(*items_caro)
-            items_task = asyncio.create_task(add_to_db(items))
-    tasks = set(asyncio.all_tasks()) - {asyncio.current_task()}
-    for task in tasks:
-        await task
+    t = 0
+    while t < 10:
+        people_number = get_people_count()
+        if people_number >= max_people:
+            print(f'Downloaded {people_number} people from {max_people}')
+            break
+        else:
+            if t == 0:
+                start_, stop = 1, max_people + 1
+            else:
+                start_, stop = stop, stop + CHUNK_SIZE
+            async with ClientSession() as client:
+                for id_chunk in chunked(range(start_, stop), CHUNK_SIZE):
+                    items_caro = [get_item(i, client) for i in id_chunk]
+                    items = await asyncio.gather(*items_caro)
+                    items_task = asyncio.create_task(add_to_db(items))
+            tasks = set(asyncio.all_tasks()) - {asyncio.current_task()}
+            for task in tasks:
+                await task
+            t += 1
 
 
 start = datetime.datetime.now()
